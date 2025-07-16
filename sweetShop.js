f38b17d73073c278855a228448994f21add889f4
kripal1825 <kripalsinhzala46@gmail.com>
2025-07-16 17:04:11 +0530

    feat:add Swweet

---
+++ sweetShop.js
@@ -0,0 +1,18 @@
+class SweetShop {
+  constructor() {
+    this.inventory = new Map(); // key: sweet ID, value: sweet object
+  }
+
+  addSweet(sweet) {
+    if (this.inventory.has(sweet.id)) {
+      throw new Error('Sweet with this ID already exists.');
+    }
+    this.inventory.set(sweet.id, sweet);
+  }
+
+  getAllSweets() {
+    return Array.from(this.inventory.values());
+  }
+}
+
+module.exports = SweetShop;
