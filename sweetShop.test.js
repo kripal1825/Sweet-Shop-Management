59a016258b403c82b427579d91024f93d8d47af8
kripal1825 <kripalsinhzala46@gmail.com>
2025-07-16 17:02:18 +0530

    test:add sweet

---
+++ sweetShop.test.js
@@ -0,0 +1,46 @@
+const SweetShop = require('./sweetShop');
+
+describe('SweetShop - Add Sweet', () => {
+  let shop;
+
+  beforeEach(() => {
+    shop = new SweetShop();
+  });
+
+  test('should add a new sweet to the inventory', () => {
+    const sweet = {
+      id: 1001,
+      name: 'Kaju Katli',
+      category: 'Nut-Based',
+      price: 50,
+      quantity: 20
+    };
+
+    shop.addSweet(sweet);
+    const allSweets = shop.getAllSweets();
+
+    expect(allSweets.length).toBe(1);
+    expect(allSweets[0]).toEqual(sweet);
+  });
+
+  test('should not allow duplicate sweet IDs', () => {
+    const sweet1 = {
+      id: 1001,
+      name: 'Kaju Katli',
+      category: 'Nut-Based',
+      price: 50,
+      quantity: 20
+    };
+
+    const sweet2 = {
+      id: 1001,
+      name: 'Gulab Jamun',
+      category: 'Milk-Based',
+      price: 30,
+      quantity: 10
+    };
+
+    shop.addSweet(sweet1);
+    expect(() => shop.addSweet(sweet2)).toThrow('Sweet with this ID already exists.');
+  });
+});
