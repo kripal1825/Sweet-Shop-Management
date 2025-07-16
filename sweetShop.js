b404d6a43129e459b22a5dab9a9d5b93811b353f
kripal1825 <kripalsinhzala46@gmail.com>
2025-07-16 17:07:32 +0530

    feat:delete sweet

--- sweetShop.js
+++ sweetShop.js
@@ -13,6 +13,14 @@ class SweetShop {
   getAllSweets() {
     return Array.from(this.inventory.values());
   }
+
+    deleteSweet(id) {
+    if (!this.inventory.has(id)) {
+      throw new Error('Sweet with this ID does not exist.');
+    }
+    this.inventory.delete(id);
+  }
+
 }
 
 module.exports = SweetShop;
